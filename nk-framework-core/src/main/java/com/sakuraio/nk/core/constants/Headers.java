03f61dc962def95fbce4fc0293c412743ab8f222
nekoimi <nekoimime@gmail.com>
2022-10-03 12:31:16 +0800

    update 添加请求header

--- nk-framework-core/src/main/java/com/sakuraio/nk/core/constants/Headers.java
+++ nk-framework-core/src/main/java/com/sakuraio/nk/core/constants/Headers.java
@@ -6,7 +6,9 @@ package com.sakuraio.nk.core.constants;
  * @author nekoimi 2022/10/03
  */
 public final class Headers {
-    public static final String TRACE_ID = "X-Trace-ID";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String AUTH_TYPE = "X-Auth-Type";
+    public static final String TRACE_ID = "X-Trace-Id";
 
     private Headers() {
     }
