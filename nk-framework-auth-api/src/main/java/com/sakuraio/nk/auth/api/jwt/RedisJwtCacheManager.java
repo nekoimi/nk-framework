3788a1be08fb2cddb355089463df998b3f0fd5a8
nekoimi <nekoimime@gmail.com>
2022-10-15 20:50:01 +0800

    update 添加jwt缓存接口

--- nk-framework-auth-api/src/main/java/com/sakuraio/nk/auth/api/jwt/RedisJwtCacheManager.java
+++ nk-framework-auth-api/src/main/java/com/sakuraio/nk/auth/api/jwt/RedisJwtCacheManager.java
@@ -11,21 +11,23 @@ public class RedisJwtCacheManager implements JwtCacheManager {
 
     @Override
     public void setRefresh(String expireToken, String refreshedToken) {
-
+        // TODO
     }
 
     @Override
     public String getRefreshedToken(String expireToken) {
+        // TODO
         return null;
     }
 
     @Override
     public void blackAdd(String token) {
-
+        // TODO
     }
 
     @Override
     public boolean blackHas(String token) {
+        // TODO
         return false;
     }
 }
