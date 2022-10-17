760888b5c6fee26e2490cdb18971390c133e276e
nekoimi <nekoimime@gmail.com>
2022-10-17 21:38:08 +0800

    update 添加json字段脱敏

---
+++ nk-framework-desensitize/src/main/java/com/sakuraio/nk/Main.java
@@ -0,0 +1,12 @@
+package com.sakuraio.nk;
+
+/**
+ * <p>${NAME}</p>
+ *
+ * @author ${USER} ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
